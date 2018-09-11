c15ec1096372eed15db75b069436f5ab64c63f09
reedF <fym_82@163.com>
2018-09-11 10:44:37 +0800

    test annotation order

---
+++ log-springboot-demo/src/main/java/com/reed/log/demo/test/AnnotationOrderTestBean.java
@@ -0,0 +1,49 @@
+package com.reed.log.demo.test;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+/**
+ *测试：
+ *1.@PostConstruct注解在bean初始化时的执行顺序
+ *2.@PostConstruct与@ConditionalOnProperty共用时，是否有效
+ * @author reed
+ *
+ */
+@Component
+@ConditionalOnProperty(prefix = "endpoints", name = "enabled", havingValue = "true", matchIfMissing = true)
+public class AnnotationOrderTestBean implements InitializingBean {
+
+	
+	public AnnotationOrderTestBean() {
+		System.out.println("InitSequenceBean: constructor");
+	}
+
+	/**
+	 * @ConditionalOnProperty对@PostConstruct控制无效
+	 */
+	@ConditionalOnProperty(prefix = "endpoints", name = "enabled", havingValue = "false", matchIfMissing = true)
+	@PostConstruct
+	public void postConstruct() {
+		System.out.println("InitSequenceBean: postConstruct");
+	}
+
+	public void initMethod() {
+		System.out.println("InitSequenceBean: init-method");
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		System.out.println("InitSequenceBean: afterPropertiesSet");
+	}
+	
+	@Bean
+	@ConditionalOnProperty(prefix = "endpoints", name = "enabled", havingValue = "true", matchIfMissing = true)
+	public AnnotationOrderTestBean genBean() {
+		System.out.println("InitSequenceBean: create new bean");
+		return new AnnotationOrderTestBean();
+	}
+}
